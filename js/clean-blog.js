1608a915988b8149c47de9fdee8809fb5c87a3b2
Meg Gutshall <meghan.gutshall@gmail.com>
2019-09-29 17:32:55 -0400

    Change navbar js

--- js/clean-blog.js
+++ js/clean-blog.js
@@ -1029,7 +1029,7 @@ $(function() {
 
 // Navigation Scripts to Show Header on Scroll-Up
 jQuery(document).ready(function($) {
-    var MQL = 1170;
+    var MQL = 768;
 
     //primary navigation slide-in effect
     if ($(window).width() > MQL) {
